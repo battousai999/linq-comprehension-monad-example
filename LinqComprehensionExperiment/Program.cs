a257940ee02aeee3c531827df411d8dbb69e8a57
Kurtis Jones <kurtis.jones@extendhealth.com>
2018-08-31 21:09:23 -0600

    Add project files.

---
+++ LinqComprehensionExperiment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinqComprehensionExperiment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
